464e3e4406464cb8986c2a315d0dbf6044847d51
yerong <241761025@qq.com>
2020-01-14 09:15:12 +0800

    no message

--- start/src/chapter2.js
+++ start/src/chapter2.js
@@ -3,5 +3,20 @@
 定义组件最简单的方式就是编写 JavaScript 函数：
 */
 function Welcome(props) {
-    return <h1>Hello, {props.name}</h1>;
+    return (<h1>Hello, {props.name}</h1>);
 }
+
+function App() {
+    return (
+        <div>
+        <Welcome name="Sara" />
+        <Welcome name="Cahal" />
+        <Welcome name="Edite" />
+        </div>
+);
+}
+
+ReactDOM.render(
+<App />,
+    document.getElementById('root')
+);
